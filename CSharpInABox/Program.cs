6d04e6ca7f7c2058b5843547e8637988b7a62650
Gary James <gjames@edgetechacademy.edu>
2020-09-29 09:16:54 -0500

    added questions for students

--- CSharpInABox/Program.cs
+++ CSharpInABox/Program.cs
@@ -17,8 +17,18 @@ namespace CSharpInABox
 
             //  Using the LINQ method ForEach print all Volcanoes since 2015
             v2015.ToList().ForEach(v => Console.WriteLine(v));
+
+            //  Using LINQ count the deaths since 2015
             int deaths = v2015.Sum(v => v.Deaths);
             Console.WriteLine($"Since 1995 there have been {deaths} deaths from {v2015.Count()} eruptions");
+
+            //  get all volcanoes for the 18th century 
+            //  what is the average number of deaths where the number of deaths was greater than 0
+            //  what was the total number of deaths 
+            //  what was the max elevation
+            //  order the volcanoes by elevation tallest to shortest
+            //  what was the 'shortest' volcanoe (Last)
+            //  get a list of the the 5th to 10th tallest volcanoes (Skip and Take)
         }
     }
 }
